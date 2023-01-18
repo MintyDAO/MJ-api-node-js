f53a15d64a249993caf262c0c19af89bba32cc67
RuslanMirov <ruslan777mirov@gmail.com>
2023-01-18 13:43:18 +0200

    Update

---
+++ test.js
@@ -0,0 +1,9 @@
+// const users = require('./DB_logic/users.js')
+//
+// const email = "mirovruslanf@gmail.com"
+// const emailHash = Buffer.from(email).toString('base64')
+//
+// users.registerUser(
+//   email,
+//   emailHash
+// )
