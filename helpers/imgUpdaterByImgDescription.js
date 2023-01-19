e3b68b425ef4e7538af94e98cb73400b4d58930d
RuslanMirov <ruslan777mirov@gmail.com>
2023-01-19 20:12:15 +0200

    Update

---
+++ helpers/imgUpdaterByImgDescription.js
@@ -0,0 +1,37 @@
+
+const dbManager = require('../dbManager.js')
+const fetchAtachmentsFromChanel = require('./fetchAtachmentsFromChanel')
+
+exports.imgUpdaterByImgDescription = async (authorization, channelid, limit) => {
+  // requests
+  let requests = dbManager.getAllRequests()
+
+  // exit if no pending requets
+  if(!requests)
+    return
+
+  let searchItems = Object.keys(requests);
+
+  // atachments
+  let jsonObjects = await fetchAtachmentsFromChanel(authorization, channelid, limit)
+
+
+  // compare pending requests descriptions with latest 50 messages
+  jsonObjects.forEach(jsonObject => {
+    try{
+      let match = searchItems.find(item =>  {
+
+        const fileName = String(jsonObject.filename).toLowerCase()
+        const key = String(item.replace(/\s/gi, "_")).toLowerCase()
+
+        // update if found match
+        if(fileName.includes(key)){
+          console.log("Update for description", item, "uri", jsonObject.url)
+          const emailHash = dbManager.getUserByRequest(item)
+          dbManager.addUserImagesByEmailHashId(emailHash, jsonObject.url)
+          dbManager.deleteUserByDescription(item)
+        }
+      });
+    }catch(e){}
+  });
+}
