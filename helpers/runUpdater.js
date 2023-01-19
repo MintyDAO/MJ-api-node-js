e038df2cf4a80119b3f45aba7006aa81f431e706
RuslanMirov <ruslan777mirov@gmail.com>
2023-01-19 20:34:05 +0200

    Update

--- helpers/runUpdater.js
+++ helpers/runUpdater.js
@@ -4,12 +4,12 @@ const config = require('../config.js')
 
 module.exports = () => {
   // run at start
-  imgUpdaterByImgDescription(process.env.DISCORD_OAUTH_TOKEN, authorization, config.chanelId, 50)
+  imgUpdaterByImgDescription(process.env.DISCORD_OAUTH_TOKEN, config.chanelId, 50)
 
   // run as interval
   setInterval(
     function () {
-      imgUpdaterByImgDescription(process.env.DISCORD_OAUTH_TOKEN, authorization, config.chanelId, 50)
+      imgUpdaterByImgDescription(process.env.DISCORD_OAUTH_TOKEN, config.chanelId, 50)
     },
   config.updatesPeriod);
 }
